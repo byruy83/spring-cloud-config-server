52d62605fe727d51270b1fec1f206287331ba8a6
YongHeeYoon <byruy83@musinsa.com>
2024-07-20 19:33:41 +0900

    add

---
+++ src/test/kotlin/com/yonghee/spring/cloud/config/server/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yonghee.spring.cloud.config.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
